477d35da5fa37fb30fb005ba65e4f0939ed7bf6b
TCHETOVSKY <semion.tchetovsky@gmail.com>
2025-12-20 13:53:42 +0100

    comment main

--- master_lhc/main.py
+++ master_lhc/main.py
@@ -1,7 +1,9 @@
 # libraries
-from interface.masterWindow import MasterWindow
 from PyQt6.QtWidgets import QApplication
 import sys
+
+# project
+from interface.masterWindow import MasterWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
