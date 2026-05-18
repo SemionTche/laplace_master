0ad437ee09930eaa6f3137efd3887a9447152606
Semion <semion.tchetovsky@gmail.com>
2026-05-18 16:27:36 +0200

    brain solid

--- laplace_master/client/brain.py
+++ laplace_master/client/brain.py
@@ -37,15 +37,18 @@ class Brain(QObject):
         
         self.suggestions = []  # candidates suggested by the optimizer
         self.results = []      # collected results from the diagnostics
-        self.obj_spec = {}
+        self.obj_spec = {}     # address of the objectives and keys associated
 
-        self.current = None   # Currently evaluated sample
-        self.waiting = False  # True while waiting for a measurement
-        self.motion_pending = False  # doing a measurement (motors moving)
+        self.current = None                 # Currently evaluated sample
+        self.waiting: bool = False          # boolean indicating if we are in a measurement process
+        self.motion_pending: bool = False   # boolean indicating if motors are moving or expected to move
 
-        self.opt_address = "Unknown"  # Address of the OPT server
-        self.motors: dict[str, list[dict]] = {}   # mask to determine which motor can move and what was the position when state changed
+        self.opt_address: str = "Unknown"        # Address of the OPT server
+        self.motors: dict[str, list[dict]] = {}  # mask to determine which motor can move and what was the position when state changed
         
+        self.shot_number_from_diags = {}    # the diagnostic addresses and the shot number they sent
+
+        ### loading tolerances
         self.tolerance_gas = get_from_config(
             module="opt",
             item="tolerance_gas",
@@ -59,11 +62,93 @@ class Brain(QObject):
             default_value=1e-3,
             type=float
         )
-        self.shot_number = -1
         
         log.debug(f"Tolerances loaded: tolerance gas = {self.tolerance_gas}, tolerance motors = {self.tolerance_motors}")
+
+        self.shot_number = -1                       # new shot number to come
+        self.latest_shot_number = -1                # previous shot number
+        self.new_shot_available = False             # boolean indicatif if there is a new shot
+        self.pending_motor_addresses = set()        # addresses of the motors that are still moving
+        self.expected_sources: set[str] = set()     # addresses of the diagnostics from which we are still waiting a key
+
+        # whether to add some logs that can be triggered often
+        self.is_trig_logs = get_from_config(
+            module="logs",
+            item="is_trig_logs",
+            default_value=False,
+            type=bool
+        )
+        log.debug(f"Trig logs on." if self.is_trig_logs else "Trig logs off.")
+        
         log.info("Brain loaded.")
 
+
+    def on_shot(self, shot_number: int) -> None:
+        '''
+        Function made to update the shot number.
+        If the new shot number to come is not higher than the
+        last shot number, it is discarded.
+        '''
+        if shot_number <= self.latest_shot_number:  # if the new shot number to come is < or = to the last one we got
+            return                                  # ignore it, it's a duplicate or a out-of-order
+
+        log.debug(f"[Shot] new shot to come={shot_number} | shot that has just been done={self.latest_shot_number} | queued_new_shot={self.new_shot_available}")
+
+        self.latest_shot_number = shot_number
+        self.new_shot_available = True
+    
+
+    def tick(self) -> None:
+        '''
+        Define where is the master in the sampling procedure.
+        '''
+        if self.motion_pending:     # if the motor are moving
+            return                  # let the time to the device to move
+
+        if not self.waiting:                             # if we are not waiting for a diagnostic (we can start next sample)
+            if self.new_shot_available:                  # if a new shot has been recorded
+                self._next(self.latest_shot_number)      # start the next sample
+                self.new_shot_available = False          # we considered the new shot 
+
+        # we are waiting for measurement
+        # if not self.waiting:
+        #     # we are idle → start next sample if possible
+        #     self._next(self.latest_shot_number)
+        #     return
+
+        # otherwise we are waiting for a diagnostic
+        if self._can_finalize():                # verify if the sample has finished since last tick           # if self._is_measurement_complete():
+            log.debug("Sample finilized.")
+            self._finalize_current_sample()
+
+    # def _is_measurement_complete(self) -> bool:
+    #     return (
+    #         self.waiting
+    #         and not self.motion_pending
+    #         and hasattr(self, "expected_sources")
+    #         and not self.expected_sources
+    #     )
+
+    def _can_finalize(self) -> bool:
+        '''
+        Verify if the sample is complete.
+        '''
+        ok = (
+            self.waiting                                  # we were waiting for a diagnostic
+            and not self.motion_pending                   # the motors are not moving
+            and hasattr(self, "expected_sources")         # the brain has an 'expected_sources' attribute
+            and len(self.expected_sources) == 0           # there is no diagnostic expected
+            and len(self.pending_motor_addresses) == 0    # there is no motor expected to move
+        )
+        # if not ok:
+        #     log.debug(
+        #         f"[Brain] finalize blocked | "
+        #         f"waiting={self.waiting}, motion_pending={self.motion_pending}, "
+        #         f"expected_sources={getattr(self, 'expected_sources', None)}, "
+        #         f"pending_motors={self.pending_motor_addresses}"                
+        #     )
+        
+        return ok
 
     def on_opt_data(self, 
                     opt_address: str, 
@@ -132,18 +217,30 @@ class Brain(QObject):
         waiting for measurements and motor control is enabled
         (or explicitly forced via `next_in_queue`).
         '''
-        # if we are waiting for a measure 
-        if self.waiting:
-            return         # don't look for the next suggestion
-
-        if self.motion_pending:
-            return
-
         # Do not proceed if motors are not enabled
         # unless we explicitly ask for an element in the suggestions
         if not (self.motor_control_enabled or next_in_queue is not None):
             return
+        
+        if shot_number < 0:
+            return
 
+        if not self.new_shot_available:
+            return
+
+        # if we are waiting for a measure 
+        if self.waiting:
+            if self.is_trig_logs:
+                log.debug(f"The method _next was triggered while we were still waiting for a diagnostic (shotnumber {self.shot_number}).\n"
+                          f"The shot number {shot_number} is then dropped from master.")
+            return         # don't look for the next suggestion
+
+        if self.motion_pending:
+            if self.is_trig_logs:
+                log.debug(f"The method _next was triggered while we were still moving the motors (shot number {self.shot_number}).\n"
+                          f"The shot number {shot_number} is then dropped from master.")
+            return
+    
         if not self.suggestions:                        # if there is no suggestion
             log.info("No suggestion available.")        # send the results
             return                                      # get out of the function
@@ -155,12 +252,15 @@ class Brain(QObject):
 
         self.current = self.suggestions.pop(next_in_queue)  # get the current point to sample and pop it from the suggestions
         self.queue_updated.emit(self.suggestions, self.obj_spec)
-        self.waiting = True                     # we start to wait for a measure
-        self.motion_pending = True
-
+        self.waiting = True                     # we start to wait for a measure (some diagnostics)
+        self.motion_pending = True              # we need to move motors
         self.current_measurements = {}          # gather the measures
-        self.expected_sources = set(self.obj_spec.keys())
 
+        self.pending_motor_addresses = set(self.current["inputs"].keys())  # addresses of the motors to move
+        self.expected_sources = set(self.obj_spec.keys())                  # addresses of the diagnostics we are waiting for
+
+
+        # filter the allowed motors
         inputs = {}
         for addr, targets in self.current["inputs"].items():
             
@@ -184,7 +284,8 @@ class Brain(QObject):
             inputs[addr] = filtered
         
         log.info("Measuring inputs:\n"
-                    f"{json_style(inputs)}")
+                    f"{json_style(inputs)}\n"
+                    f"It should be related with the next shot number to come ({self.shot_number})")
 
         self.client_manager.sample_point(inputs)  # send the imputs to control system servers
         self.commanded_inputs = inputs            # what was asked to the motors 
@@ -192,6 +293,7 @@ class Brain(QObject):
     def on_motor_position_update(self, address: str, positions: dict):
         # update the motor mask
         motor_list = self.motors.get(address)
+
         if motor_list:
             for i, pos in enumerate(positions.get("positions", [])):
                 motor_list[i]["position"] = pos
@@ -202,8 +304,10 @@ class Brain(QObject):
         target = self.commanded_inputs
 
         if self._motors_match_target(address, positions, target):
-            log.info("Motors reached target. Starting measurement phase.")
-            self.motion_pending = False
+            self.pending_motor_addresses.discard(address)
+            if not self.pending_motor_addresses:
+                log.info("Motors reached target. Starting measurement phase.")
+                self.motion_pending = False
 
 
     def _motors_match_target(self, address, current, target):        
@@ -213,22 +317,27 @@ class Brain(QObject):
         if target_positions is None:
             return False
 
-        if len(current_positions) != len(target_positions):
+        if len(current_positions) < len(target_positions):
             return False
-        
+
         for c, t in zip(current_positions, target_positions):
+
             if t is None:
                 continue
             
             if self.client_manager.server_devices[address] == DEVICE_GAS:
-                if abs(c - t) <= self.tolerance_gas:
-                    return True
+                device = "Gas"
+                if abs(c - t) > self.tolerance_gas:
+                    return False
             
             elif self.client_manager.server_devices[address] == DEVICE_MOTOR:
-                if abs(c - t) <= self.tolerance_motors:
-                    return True
-
-        return False
+                device = "Motor"
+                if abs(c - t) > self.tolerance_motors:
+                    return False
+        
+        log.debug(f"{device} tolerance passed, for address {address}.")
+        
+        return True
 
 
     def on_measurement(self, 
@@ -238,8 +347,7 @@ class Brain(QObject):
         Process a measurement received from a diagnostic server.
 
         Measurements are collected until all expected sources have
-        responded for the current sample. Once complete, the sample
-        is finalized.
+        responded for the current sample.
 
         Args:
             address: (str)
@@ -248,28 +356,55 @@ class Brain(QObject):
             data: (dict)
                 Measured values for the current sample.
         '''
+        if not data:
+            return
+        
+        # print(f'data from on_measurement = {data}')
         if not self.waiting:               # if we are not waiting for a measure
+            if self.is_trig_logs and data:
+                log.debug(f"The method on_measurement was triggered while we were not waiting for a diagnostic (shot number {self.shot_number}).")
             return                         # we do not continue
 
         if self.motion_pending:
+            if self.is_trig_logs and data:
+                log.debug(f"The method on_measurement was triggered while we were still moving motor (shot number {self.shot_number}).\n"
+                          f"The diagnostic was then dropped.")
+                if "shot_number" in data.keys():
+                    log.debug(f"The dropped diagnostic had shot number {data['shot_number']}")
             return
 
         if address not in self.obj_spec:   # if a measure is received from an unexpected address
+            if self.is_trig_logs and data:
+                log.debug(f"The method on_measurement was triggered but we do not have the adress ({address}) of this objective in our list of objectives ({self.obj_spec}).")
             return                         # ignore it
 
         values = data
         if not isinstance(values, dict):
+            log.debug(f"The type of the data ({type(values)}) received from the diagnostic {address} is not {dict}.")
             return
         
-        log.info(
-            f"Measurement received from {address}:\n"
-                f"{json_style(values)}"
-        )
+        shot = values.get("shot_number")
+        if shot is None:
+            log.debug("Missing shot_number, dropping diagnostic")
+            return
+        
+        if shot != self.shot_number:
+            log.warning(f"Dropping diagnostic: expected {self.shot_number}, got {shot}")
+            return
+
+        if values:
+            log.info(
+                f"Measurement received from {address}:\n"
+                    f"{json_style(values)}"
+            )
 
         # Initialize storage
-        self.current_measurements.setdefault(address, {})  # create a key with empty dict in current_measurements
+        # self.current_measurements.setdefault(address, {})  # create a key with empty dict in current_measurements
 
-        expected_keys = self.obj_spec[address]  # now just a list of objective names
+        if address not in self.current_measurements:
+            self.current_measurements[address] = {}
+
+        expected_keys = self.obj_spec[address]  # a list of objective names
         
         for k in expected_keys:
             if k in values:
@@ -278,11 +413,13 @@ class Brain(QObject):
         # Check completion for this address
         if len(self.current_measurements[address]) == len(expected_keys):
             self.expected_sources.discard(address)
+        
+        self.shot_number_from_diags[address] = values["shot_number"]
 
         # Finalize sample if everything is collected
-        if not self.expected_sources:
-            log.info("All diagnostic measurements collected. Finalizing sample.")
-            self._finalize_current_sample()
+        # if not self.expected_sources:
+        #     log.info("All diagnostic measurements collected. Finalizing sample.")
+        #     self._finalize_current_sample()
 
 
     def _finalize_current_sample(self) -> None:
@@ -294,6 +431,7 @@ class Brain(QObject):
         are sent back to the optimizer.
         '''
         if not isinstance(self.current, dict):
+            log.debug(f"Impossible to finalize the current sample, self.current must be a {dict}, not ({type(self.current)})")
             return
         
         self.results.append({
@@ -301,12 +439,18 @@ class Brain(QObject):
             "outputs": self.current_measurements,
             "batch": self.current["batch"],
             "candidate": self.current["candidate"],
-            "shot_number": self.shot_number
+            "shot_number_from_master": self.shot_number,
+            "shot_number_from_diags": self.shot_number_from_diags
         })
+
+        for key in self.shot_number_from_diags.keys():
+            if self.shot_number != self.shot_number_from_diags[key]:
+                log.error("The shot number from the master and the diagnostics are different.")
 
         self.current = None
         self.waiting = False
         self.shot_number = -1
+        self.shot_number_from_diags = {}
         self.queue_updated.emit(self.suggestions, self.obj_spec)
 
         # if self.suggestions:
@@ -322,6 +466,7 @@ class Brain(QObject):
         Send collected batch results to the optimization server.
         '''
         if self.opt_address is None:
+            log.debug(f"The optimizer adress is {None}. Impossible to send back results.")
             return
 
         if self.results:
@@ -347,7 +492,7 @@ class Brain(QObject):
         self.motor_control_enabled = enabled
         
         # if enabled:         # if motors can be drive
-        #     self._next()    # get the next sample
+        #     self._next(shot_number=-1)    # get the next sample
 
 
     def set_motor_enabled(self, 
